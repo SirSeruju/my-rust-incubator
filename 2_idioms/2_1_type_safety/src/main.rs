985a054898f496955460123f803cd4c4c76af8cc
Seruju <sir.serujU@yandex.ru>
2023-11-21 04:55:50 +0000

    Task

--- 2_idioms/2_1_type_safety/src/main.rs
+++ 2_idioms/2_1_type_safety/src/main.rs
@@ -1,3 +1,74 @@
+// +-----+              +-------------+            +-----------+
+// | New |--publish()-->| Unmoderated |--allow()-->| Published |
+// +-----+              +-------------+            +-----------+
+//                            |                          |
+//                          deny()                    delete()
+//                            |       +---------+        |
+//                            +------>| Deleted |<-------+
+//                                    +---------+
+
+// States
+#[derive(Debug)]
+struct New;
+#[derive(Debug)]
+struct Unmoderated;
+#[derive(Debug)]
+struct Published;
+#[derive(Debug)]
+struct Deleted;
+
+// Transitions
+impl New {
+    fn publish(self) -> Unmoderated {
+        Unmoderated {}
+    }
+}
+
+impl Unmoderated {
+    fn allow(self) -> Published {
+        Published {}
+    }
+    fn deny(self) -> Deleted {
+        Deleted {}
+    }
+}
+
+impl Published {
+    fn delete(self) -> Deleted {
+        Deleted {}
+    }
+}
+
 fn main() {
-    println!("Implement me!");
+    println!(
+        "\
++-----+              +-------------+            +-----------+
+| New |--publish()-->| Unmoderated |--allow()-->| Published |
++-----+              +-------------+            +-----------+
+                           |                          |
+                         deny()                    delete()
+                           |       +---------+        |
+                           +------>| Deleted |<-------+
+                                   +---------+"
+    );
+    println!();
+
+    println!("First path: New -> Unmoderated -> Deleted");
+    let state = New {};
+    println!("Initial: {:?}", state);
+    let state = state.publish();
+    println!("after publish: {:?}", state);
+    let state = state.deny();
+    println!("after deny: {:?}", state);
+    println!();
+
+    println!("Second path: New -> Unmoderated -> Published -> Deleted");
+    let state = New {};
+    println!("Initial: {:?}", state);
+    let state = state.publish();
+    println!("after publish: {:?}", state);
+    let state = state.allow();
+    println!("after allow: {:?}", state);
+    let state = state.delete();
+    println!("after delete: {:?}", state);
 }
